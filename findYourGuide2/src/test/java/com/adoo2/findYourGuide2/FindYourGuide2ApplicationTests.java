80d43eaf1688f34c4978d03fce75107b24bff6c4
ivnperez <playgamer998@hotmail.com>
2024-06-26 17:50:18 -0300

    Se modifican las configuraciones

--- findYourGuide2/src/test/java/com/adoo2/findYourGuide2/FindYourGuide2ApplicationTests.java
+++ findYourGuide2/src/test/java/com/adoo2/findYourGuide2/FindYourGuide2ApplicationTests.java
@@ -1,5 +1,5 @@
 package com.adoo2.findYourGuide2;
-
+/* 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +10,4 @@ class FindYourGuide2ApplicationTests {
 	void contextLoads() {
 	}
 
-}
+} */
